b98d047ba8e5e8ebc03021bbb5abf7fd5b11a8f6
Josedlca <jose.dlcruz.ayala@gmail.com>
2019-10-11 17:54:43 -0500

    styles for user tables

--- src/components/NameFileComponent.vue
+++ src/components/NameFileComponent.vue
@@ -1,20 +1,31 @@
 <template>
-	<div class="w-full h-10 flex border-t-2 border-b-2">
-		<div class=" w-2/12 flex items-center justify-left font-bold" v-for="item of allTitles" :key="item.id">
-			{{item}}
+	<div class="w-full h-10 flex border-t-2 pl-1 border-b-2">
+		<div class="w-12 flex items-center justify-left font-bold">
+			ID
+		</div>
+		<div class=" w-24 flex items-center justify-left font-bold">
+			Name
+		</div>
+		<div class=" w-2/12 flex items-center justify-left font-bold">
+			Lastname
+		</div>
+		<div class=" w-3/12 flex items-center justify-left font-bold">
+			Email
+		</div>
+		<div class=" w-2/12 flex items-center justify-left font-bold">
+			Phone
+		</div>
+		<div class=" w-2/12 flex items-center justify-left font-bold">
+			Username
+		</div>
+		<div class=" w-2/12 flex items-center justify-left font-bold">
+			Actions
 		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'NameFileComponent',	
-		data(){
-			return{
-				allTitles:[
-					"ID", "First", "Last", "Email", "Phone","Actions"
-				]
-			}
-		}
+		name: 'NameFileComponent',
 	}
 </script>
--- src/views/UserTable.vue
+++ src/views/UserTable.vue
@@ -2,22 +2,37 @@
 	<div class="w-full">
 		<div class="w-10/12 mx-auto">
 			<NameFileComponent />
-			<div v-for="(item, index) of userInfoData" :key="item.id">
-				<p>{{item.name}}</p>
-				<p>{{item.lastname}}</p>
-				<p>{{item.email}}</p>
-				<p>{{item.username}}</p>
-				<p>{{item.phone}}</p>
-				<router-link :to="{name: 'editUser', params: {id:index}}">
-					<svg class="w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-						<path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/>
-					</svg>
-				</router-link>
-				<button @click="deleteUserInfo(index)">
-					<svg class="w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-						<path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/>
-					</svg>
-				</button>
+			<div class="w-full flex h-10 border-b pl-1" v-for="(item, index) of userInfoData" :key="item.id">
+				<div class="w-12 flex items-center justify-left font-bold">
+					{{index}}
+				</div>
+				<div class="w-24 flex items-center justify-left">
+					{{item.name}}
+				</div>
+				<div class="w-2/12 flex items-center justify-left">
+					{{item.lastname}}
+				</div>
+				<div class="w-3/12 flex items-center justify-left">
+					{{item.email}}
+				</div>
+				<div class="w-2/12 flex items-center justify-left">
+					{{item.username}}
+				</div>
+				<div class="w-2/12 flex items-center justify-left">
+					{{item.phone}}
+				</div>
+				<div class="w-2/12 flex items-center justify-left">
+					<router-link class="bg-yellow-600 rounded-lg h-8 px-4 flex items-center mr-3" :to="{name: 'editUser', params: {id:index}}">
+						<svg class="w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+							<path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/>
+						</svg>
+					</router-link>
+					<button class="bg-red-600 rounded-lg h-8 px-4 flex items-center" @click="deleteUserInfo(index)">
+						<svg class="w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+							<path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/>
+						</svg>
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
